a1d10c3a43f044c0d8cea24d96c505357f3bb07d
Tim M <timothy.romero.manas@gmail.com>
2021-04-05 19:41:10 -0400

    Adding store

---
+++ frontend/src/store.js
@@ -0,0 +1,23 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+
+const reducer = combineReducers({})
+
+// This is where we get our cartItes, tokens, userInfo
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromStorage,
+    shippingAddress: shippingAddressFromStorage,
+  },
+  userLogin: { userInfo: userInfoFromStorage },
+}
+const middleware = [thunk]
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+)
+
+export default store
